d137b4bf2d9f85c6739657fd9cc8cbf1b8693587
Nidhi Sharma <nidhi.aksharma@gmail.com>
2017-05-27 17:27:19 +0530

    Test Commit 3.

--- PayReminder/ViewController.swift
+++ PayReminder/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //test change
-        //test change 2
+        //Test change
+        //Running change 2
     }
 
     override func didReceiveMemoryWarning() {
